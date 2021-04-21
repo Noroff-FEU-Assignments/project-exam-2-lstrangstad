663a93498b18fda11ece7e2720acf6f761b46e57
Linus <linusstrangstad@gmail.com>
2021-04-21 15:20:38 +0200

    Feature: Added route paths & AuthProvider

--- holidayze-react/src/App.js
+++ holidayze-react/src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 import Home from "./pages/Home";
+import Stays from "./pages/Stays";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/stays" exact component={Stays} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthProvider>
   );
 }
 
