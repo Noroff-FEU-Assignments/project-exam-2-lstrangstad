311db71100fcbedaf258b3358f9496c1a936b686
Linus <linusstrangstad@gmail.com>
2021-04-21 15:22:12 +0200

    Feature: Homepage markup

--- holidayze-react/src/pages/Home.js
+++ holidayze-react/src/pages/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
-      <h1>Hello Home</h1>
-    </>
+    <div className="home">
+      <div>
+        <h1>Find your accommodation in Bergen now</h1>
+      </div>
+      <div>
+        <h2>Search Hotel</h2>
+        <input type="text" />
+        <button>Go</button>
+      </div>
+    </div>
   );
 };
 
