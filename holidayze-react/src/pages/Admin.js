54877017dbd60741e2bd0827ab67bc0e98ce0365
Linus <linusstrangstad@gmail.com>
2021-04-23 11:34:53 +0200

    Feat: Admin page markup

---
+++ holidayze-react/src/pages/Admin.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Admin = () => {
+  const [error, setError] = useState(null);
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default Admin;
