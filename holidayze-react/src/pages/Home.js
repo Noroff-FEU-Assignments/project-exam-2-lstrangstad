870cdb72fceb330e23b9d23a06b3b6324da7573b
Linus <linusstrangstad@gmail.com>
2021-04-23 11:36:02 +0200

    Feat: Home page markup

--- holidayze-react/src/pages/Home.js
+++ holidayze-react/src/pages/Home.js
@@ -1,6 +1,31 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
 
 const Home = () => {
+  const [hotels, setHotels] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [isFiltered, setIsFiltered] = useState(false);
+  useEffect(() => {
+    const fetchHotels = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/hotels`);
+        setHotels(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchHotels();
+    console.log(hotels);
+  }, []);
+
+  const handleFilter = (e) => {
+    let filteredHotels = hotels.filter((hotel) => {
+      return hotel.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setFilter(filteredHotels);
+    setIsFiltered(true);
+  };
   return (
     <div className="home">
       <div>
@@ -8,7 +33,17 @@ const Home = () => {
       </div>
       <div>
         <h2>Search Hotel</h2>
-        <input type="text" />
+        <input type="text" onChange={handleFilter} />
+        <select name="names" id="">
+          {hotels?.map((hotel, idx) => {
+            return (
+              <option key={idx} value={hotel.name}>
+                {hotel.name}
+              </option>
+            );
+          })}
+          <option value={hotels}></option>
+        </select>
         <button>Go</button>
       </div>
     </div>
