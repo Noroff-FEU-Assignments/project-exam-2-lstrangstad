17d7f05d45226ec0a01c3c63e2e148c8e3883f8d
Linus <linusstrangstad@gmail.com>
2021-04-27 17:46:29 +0200

    Feat: Serchfilter logic

--- holidayze-react/src/pages/Home.js
+++ holidayze-react/src/pages/Home.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [hotels, setHotels] = useState([]);
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState(null);
   const [isFiltered, setIsFiltered] = useState(false);
+  const [toggle, setToggle] = useState("closed");
+
   useEffect(() => {
     const fetchHotels = async () => {
       try {
@@ -25,7 +28,14 @@ const Home = () => {
     });
     setFilter(filteredHotels);
     setIsFiltered(true);
+
+    if (e.target.value === "") {
+      setToggle("closed");
+    } else {
+      setToggle("toggled");
+    }
   };
+
   return (
     <div className="home">
       <div>
@@ -34,17 +44,26 @@ const Home = () => {
       <div>
         <h2>Search Hotel</h2>
         <input type="text" onChange={handleFilter} />
-        <select name="names" id="">
-          {hotels?.map((hotel, idx) => {
-            return (
-              <option key={idx} value={hotel.name}>
-                {hotel.name}
-              </option>
-            );
-          })}
-          <option value={hotels}></option>
-        </select>
-        <button>Go</button>
+        <div className={toggle}>
+          {isFiltered ? (
+            <div>
+              <ul>
+                {filter?.map((hotel) => {
+                  return (
+                    <li key={hotel.id}>
+                      <Link
+                        style={{ display: "flex" }}
+                        to={`/details/${hotel.id}`}
+                      >
+                        {hotel.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
