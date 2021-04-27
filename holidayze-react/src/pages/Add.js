9f78ebb2d603bb9bf7ca076dbc6ad2dfa64efa7d
Linus <linusstrangstad@gmail.com>
2021-04-27 17:42:52 +0200

    Feat: Changed formik to useForm

--- holidayze-react/src/pages/Add.js
+++ holidayze-react/src/pages/Add.js
@@ -2,9 +2,10 @@ import { useContext, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import AuthContext from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { useForm } from "react-hook-form";
 import useAxios from "../utils/useAxios";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addSchema } from "../utils/schemas";
 
 const Add = () => {
   const [hotel, setHotel] = useState(null);
@@ -14,55 +15,32 @@ const Add = () => {
   const [auth] = useContext(AuthContext);
   const http = useAxios();
   const history = useHistory();
-
-  const {
-    handleSubmit,
-    handleChange,
-    handleBlur,
-    values,
-    touched,
-    errors,
-  } = useFormik({
-    initialValues: {
-      name: "",
-      description: "",
-      price: "",
-      image: "",
-      adress: "",
-      rating: "",
-    },
-    addSchema: yup.object().shape({
-      name: yup.string().required("Please enter name of accomodation"),
-      description: yup
-        .string()
-        .required("Please enter description of accomodation"),
-      price: yup.number().required("Please enter price of accomodation"),
-      img: yup.string().required("Please enter img of accomodation"),
-      address: yup.string().required("Please enter address of accomodation"),
-      rating: yup.string().required("Please enter rating of accomodation"),
-    }),
-    onSubmit: async (values) => {
-      setSubmit(true);
-      setPostError(null);
-      console.log(values);
-
-      try {
-        const response = await http.post(`${BASE_URL}/hotels`, values);
-        console.log(response.data);
-        setHotel(response.data);
-        setSuccess(true);
-      } catch (err) {
-        console.log("error", err);
-      } finally {
-        setSubmit(false);
-      }
-    },
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(addSchema),
   });
+
+  const onSubmit = async (data) => {
+    setSubmit(true);
+    setPostError(null);
+    console.log(data);
+
+    try {
+      const response = await http.post(`${BASE_URL}/hotels`, data);
+      console.log(response.data);
+      setHotel(response.data);
+      setSuccess(true);
+    } catch (err) {
+      console.log("error", err);
+    } finally {
+      setSubmit(false);
+    }
+  };
+
   return (
     <div>
       <h1>Add accomodation</h1>
       <div>
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           {postError && <p>{postError}</p>}
           <fieldset className="form__fieldset" disabled={submit}>
             {success ? (
@@ -72,73 +50,39 @@ const Add = () => {
             ) : null}
             <div className="form__box">
               <input
-                value={values.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
                 name="name"
                 placeholder="Occomodation name"
+                ref={register}
               />
-              {touched.name && errors.name ? <p>{errors.name}</p> : null}
+              {errors.name && <p>{errors.name.message}</p>}
             </div>
 
             <div className="form__box">
               <input
-                value={values.description}
-                onChange={handleChange}
-                onBlur={handleBlur}
                 name="description"
                 placeholder="Description"
+                ref={register}
               />
-              {touched.description && errors.description ? (
-                <div>{errors.description}</div>
-              ) : null}
+              {errors.description && <p>{errors.description.message}</p>}
             </div>
             <div className="form__box">
-              <input
-                value={values.price}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                name="price"
-                placeholder="Price"
-              />
-              {touched.price && errors.price ? <div>{errors.price}</div> : null}
+              <input name="price" placeholder="Price" ref={register} />
+              {errors.price && <p>{errors.price.message}</p>}
             </div>
             <div className="form__box">
-              <input
-                value={values.rating}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                name="rating"
-                placeholder="Rating (9.5)"
-              />
-              {touched.rating && errors.rating ? (
-                <div>{errors.rating}</div>
-              ) : null}
+              <input name="rating" placeholder="Rating (9.5)" ref={register} />
+              {errors.rating && <p>{errors.rating.message}</p>}
             </div>
             <div className="form__box">
-              <input
-                value={values.image}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                name="image"
-                placeholder="Image"
-              />
-              {touched.image && errors.image ? <div>{errors.image}</div> : null}
+              <input name="image" placeholder="Image" ref={register} />
+              {errors.image && <p>{errors.image.message}</p>}
             </div>
             <div className="form__box">
-              <input
-                value={values.adress}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                name="adress"
-                placeholder="Address"
-              />
-              {touched.adress && errors.adress ? (
-                <div>{errors.adress}</div>
-              ) : null}
+              <input name="adress" placeholder="Address" ref={register} />
+              {errors.adress && <p>{errors.adress.message}</p>}
             </div>
             <button className="form__button btn" type="submit">
-              {submit ? "Loggin in..." : "Login"}
+              {submit ? "Adding..." : "Add"}
             </button>
           </fieldset>
         </form>
