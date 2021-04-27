ad37c2c7ba88f5cb8c173a0fa9d2283b6812c439
Linus <linusstrangstad@gmail.com>
2021-04-27 17:46:04 +0200

    Feat: fetchinng & rendering details

--- holidayze-react/src/pages/Details.js
+++ holidayze-react/src/pages/Details.js
@@ -1,7 +1,38 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
+import Modal from "../components/Modal";
+
 const Details = () => {
+  const [hotel, setHotel] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchHotel = async () => {
+      const response = await axios.get(`${BASE_URL}/hotels/${id}`);
+      setHotel(response.data);
+      console.log(response.data);
+    };
+    fetchHotel();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h1>{hotel.name}</h1>
+        <p>{hotel.location}</p>
+        <strong>{hotel.price}</strong>
+        <div>{hotel.rating}</div>
+        <Modal />
+      </div>
+      <div>
+        <h2>Overview</h2>
+        <p>{hotel.description}</p>
+      </div>
     </div>
   );
 };
