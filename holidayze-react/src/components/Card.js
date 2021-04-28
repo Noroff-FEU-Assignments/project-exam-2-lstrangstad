905043b892b83116d6c8a5f1dc8b624cee8ce56f
Linus <linusstrangstad@gmail.com>
2021-04-28 20:05:26 +0200

    Feat: Card classnames

--- holidayze-react/src/components/Card.js
+++ holidayze-react/src/components/Card.js
@@ -4,15 +4,16 @@ const Card = (props) => {
   const { name, image, location, price, rating, id } = props;
 
   return (
-    <>
-      <Link to={`/details/${id}`}>
-        <img src={image} alt={name} />
-        <h2>{name}</h2>
-        <p>{location}</p>
-        <strong>Price: {price}kr</strong>
-        <div>{rating}</div>
+    <div className="card">
+      <Link className="card__link" to={`/details/${id}`}>
+        <img className="card__image" src={image} alt={name} />
+
+        <h2 className="card__name">{name}</h2>
+        <p className="card__location">{location}</p>
+        <p className="card__price">Price: {price}kr</p>
+        <div className="card__rating">{rating}</div>
       </Link>
-    </>
+    </div>
   );
 };
 
