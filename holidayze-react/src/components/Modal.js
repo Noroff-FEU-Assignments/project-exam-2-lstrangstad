6bf015954d53e00a2c41e0ec33d512b307925909
Linus <linusstrangstad@gmail.com>
2021-05-05 18:12:54 +0200

    Feat: New modal

--- holidayze-react/src/components/Modal.js
+++ holidayze-react/src/components/Modal.js
@@ -7,43 +7,30 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { bookingSchema } from "../utils/schemas";
 import { BASE_URL } from "../utils/constants";
-import useAxios from "../utils/useAxios";
-
-const myModal = () => {};
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
+import axios from "axios";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   paper: {
-    position: "absolute",
-    width: 400,
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+    maxWidth: "450px",
+    width: "350px",
   },
 }));
 
-export default function SimpleModal() {
+export default function TransitionsModal(props) {
   const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
-
-  const http = useAxios();
+  const [startDate, setStartDate] = useState(new Date().toISOString());
   const [submit, setSubmit] = useState(false);
   const [postError, setPostError] = useState(null);
   const [success, setSuccess] = useState(null);
@@ -57,12 +44,13 @@ export default function SimpleModal() {
     console.log(data);
 
     try {
-      const response = await http.post(`${BASE_URL}/bookings`, data);
+      const response = await axios.post(`${BASE_URL}/bookings`, data);
+      console.log(response.data);
     } catch (err) {
       console.log("error", err);
       setPostError(err.toString());
     } finally {
-      setSubmit(true);
+      setSubmit(false);
     }
   };
 
@@ -74,51 +62,102 @@ export default function SimpleModal() {
     setOpen(false);
   };
 
-  const [startDate, setStartDate] = useState(new Date());
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Book now</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {postError && <p className="add__error">{postError}</p>}
-        <fieldset disabled={submit}>
-          <label>Name</label>
-          <input type="text" name="name" ref={register} />
-          {errors.name && <p>{errors.name.message}</p>}
-          <input
-            onInput={(e) => setStartDate(e.target.value)}
-            id="startdate"
-            type="date"
-            name="date"
-            ref={register}
-            required
-          />
-
-          <label>Nights</label>
-          <input type="number" name="nights" ref={register} />
-          {errors.nights && <p>{errors.nights.message}</p>}
-          <label>Persons</label>
-          <input type="number" name="Persons" ref={register} />
-          {errors.Persons && <p>{errors.Persons.message}</p>}
-          <button>Send</button>
-        </fieldset>
-      </form>
-    </div>
-  );
-
-  console.log(startDate);
   return (
     <div>
-      <button type="button" onClick={handleOpen}>
+      <button className="details__btn" type="button" onClick={handleOpen}>
         Book now
       </button>
       <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
         open={open}
         onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
       >
-        {body}
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <div className="modal">
+              <h2 className="modal__header">Book now</h2>
+              <form className="modal__form" onSubmit={handleSubmit(onSubmit)}>
+                {postError && <p className="add__error">{postError}</p>}
+                <fieldset className="modal__field" disabled={submit}>
+                  <input
+                    className="modal__input"
+                    value={props.name}
+                    name="hotel_name"
+                    ref={register}
+                    type="hidden"
+                  />
+                  <input
+                    className="modal__input"
+                    value={props.price}
+                    name="price"
+                    ref={register}
+                    type="hidden"
+                  />
+                  <div className="modal__input-box">
+                    <label className="modal__label">Name: </label>
+                    <input
+                      className="modal__input"
+                      type="text"
+                      name="name"
+                      ref={register}
+                    />
+                    {errors.name && <p>{errors.name.message}</p>}
+                  </div>
+                  <div className="modal__input-box">
+                    <label className="modal__label">Arrival: </label>
+                    <input
+                      className="modal__input"
+                      onChange={(e) => setStartDate(e.target.value)}
+                      id="startdate"
+                      type="date"
+                      name="date"
+                      ref={register}
+                      required
+                    />
+                  </div>
+                  <div className="modal__input-box">
+                    <label className="modal__label">Nights: </label>
+                    <input
+                      className="modal__input"
+                      type="text"
+                      name="nights"
+                      ref={register}
+                    />
+                    {errors.nights && <p>{errors.nights.message}</p>}
+                  </div>
+                  <div className="modal__input-box">
+                    <label className="modal__label">Adults: </label>
+                    <input
+                      className="modal__input"
+                      type="text"
+                      name="adults"
+                      ref={register}
+                    />
+                    {errors.adults && <p>{errors.adults.message}</p>}
+                  </div>
+                  <div className="modal__input-box">
+                    <label className="modal__label">Children: </label>
+                    <input
+                      className="modal__input"
+                      type="text"
+                      name="children"
+                      ref={register}
+                    />
+                    {errors.children && <p>{errors.children.message}</p>}
+                  </div>
+                  <button className="modal__button">Send</button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </Fade>
       </Modal>
     </div>
   );
