8d0088712f583b76bc9c19c94378a8d2cece9722
Linus <linusstrangstad@gmail.com>
2021-05-14 23:26:29 +0200

    Feat: Adding routes

--- holidayze-react/src/App.js
+++ holidayze-react/src/App.js
@@ -10,6 +10,8 @@ import Contact from "./pages/Contact";
 import Admin from "./pages/Admin";
 import Add from "./pages/Add";
 import Details from "./pages/Details";
+import Enquiries from "./pages/Enquiries";
+import Messages from "./pages/Messages";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/login" component={Login} />
           <Route path="/admin" component={Admin} />
+          <Route path="/enquiries" component={Enquiries} />
+          <Route path="/messages" component={Messages} />
           <Route path="/add" component={Add} />
           <Route path="/details/:id" component={Details} />
         </Switch>
