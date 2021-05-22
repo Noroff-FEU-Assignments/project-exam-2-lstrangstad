fd0a4c3fe20573bea996d74e74f4e9ee925e1df8
Linus <linusstrangstad@gmail.com>
2021-05-22 16:45:01 +0200

    Fix: Image and classnames

--- holidayze-react/src/components/Card.js
+++ holidayze-react/src/components/Card.js
@@ -6,14 +6,22 @@ const Card = (props) => {
   return (
     <div className="card">
       <Link className="card__link" to={`/details/${id}`}>
-        <div className="card__img-box">
-          <img className="card__image" src={image} alt={name} />
+        <div className="card__image-box">
+          <div
+            className="card__img"
+            style={{ backgroundImage: `url(${image})` }}
+          ></div>
         </div>
         <div className="card__content">
           <h2 className="card__name">{name}</h2>
           <p className="card__location">{location}</p>
-          <p className="card__price">Price: {price}kr</p>
-          <div className="card__rating">{rating}</div>
+          <div className="card__flex">
+            <p className="card__price">Price: {price}kr</p>
+
+            <div className="card__rating">
+              <span className="card__number">{rating}</span>
+            </div>
+          </div>
         </div>
       </Link>
     </div>
