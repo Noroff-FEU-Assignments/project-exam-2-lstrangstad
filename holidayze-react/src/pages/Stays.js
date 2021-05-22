8988e40ff4bdf4ca8fbab9de0b7aba1280e1270c
Linus <linusstrangstad@gmail.com>
2021-05-22 16:51:41 +0200

    Feat: filter results

--- holidayze-react/src/pages/Stays.js
+++ holidayze-react/src/pages/Stays.js
@@ -3,9 +3,12 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import Card from "../components/Card";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { HotelRounded } from "@material-ui/icons";
 
 const Stays = () => {
   const [hotels, setHotels] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [isFiltered, setIsFiltered] = useState(false);
   const [loader, setLoader] = useState(true);
 
   useEffect(function () {
@@ -20,21 +23,50 @@ const Stays = () => {
     };
     fetchHotels();
   }, []);
-  console.log(hotels);
+
+  const handleFilter = (e) => {
+    let filteredHotels = hotels.filter((hotel) => {
+      return hotel.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setFilter(filteredHotels);
+    setIsFiltered(true);
+  };
+
+  console.log(filter);
+
   return (
     <div className="stays">
+      <h1 className="stays__header">Our hotels</h1>
+      <input type="text" className="stays__search" onChange={handleFilter} />
+      <select name="" id="">
+        <option value="">All stays</option>
+        <option value="">Hotel</option>
+        <option value="">B&B</option>
+        <option value="">Cabin</option>
+      </select>
       {loader ? (
         <div className="loader">
           <CircularProgress style={{ placeItems: "center" }} />
         </div>
       ) : (
         <>
-          <h1 className="stays__header">Our hotels</h1>
-          <div className="stays__container">
-            {hotels.map((hotel) => {
-              return <Card key={hotel.id} {...hotel} />;
-            })}
-          </div>
+          {isFiltered ? (
+            <div className="stays__container">
+              {filter.length !== 0 ? (
+                filter.map((hotel) => <Card key={hotel.id} {...hotel} />)
+              ) : (
+                <div>
+                  <p>No Hotels is matching your search...</p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="stays__container">
+              {hotels.map((hotel) => (
+                <Card key={hotel.id} {...hotel} />
+              ))}
+            </div>
+          )}
         </>
       )}
     </div>
